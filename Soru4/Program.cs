634e096a7a6667ec463fdba9080a4b9f79e36755
Yusufkr03 <yusufkr2903@gmail.com>
2024-10-16 22:11:41 +0300

    Add files via upload

---
+++ Soru4/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Toplamını ve çarpımını istediğiniz 4 sayı giriniz");
+Console.WriteLine("1. sayı: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("2. sayı: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("3. sayı: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("4. sayı: ");
+int number4 = Convert.ToInt32(Console.ReadLine());
+int result1 = number1 + number2 + number3 + number4;
+int result2 = number1 * number2 * number3 * number4;
+Console.WriteLine("Toplamları:" + result1);
+Console.WriteLine("Çarpımları:" + result2);
+Console.ReadKey();
